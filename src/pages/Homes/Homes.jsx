8ebb19d25b3a713d4b737a4bdd5adbe1b20983d1
mdosit <mdos2it@outlook.com>
2026-03-26 13:56:16 -0400

    another test push
    hey

--- src/pages/Homes/Homes.jsx
+++ src/pages/Homes/Homes.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 //temporary
-const mockUser = { name: "jim" };
+const mockUser = { name: "billy" };
 const mockHouse = { name: "the la casa house" };
 
 const tempHomeList = [
